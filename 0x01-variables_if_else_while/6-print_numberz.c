b3cac329933047456777a66cff7d2bdbc3642e44
haneenakram <nonakram@gmail.com>
2023-08-11 18:07:14 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  *main - main is the entry point
+  *Return: return 0 (succsess)
+  */
+int main(void)
+{
+	char i;
+for (i = '0' ; i < '10' ; i++)
+putchar("%d", i);
+putchar("\n");
+return (0);
+}
