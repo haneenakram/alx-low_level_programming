1460ec908090251546793d5d3e2089f1456a7ca5
haneenakram <nonakram@gmail.com>
2023-08-11 17:44:53 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stio.h>
+/**
+  *main - main is the entry point
+  *Return: return 0 (succsess)
+  */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		putchar(c);
+	char a;
+
+	for (a = 'A' ; a <= 'Z' ; a++)
+		putchar(a);
+putchar('\n');
+	return (0);
+}
