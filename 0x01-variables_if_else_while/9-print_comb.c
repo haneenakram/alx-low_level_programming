198b1811da4e6f42e8877eb2907fffba4dbf164e
haneenakram <nonakram@gmail.com>
2023-08-11 18:20:44 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *main - main is the entry point
+  *Return: return 0 (succsess)
+  */
+int main(void)
+{
+char c;
+for (c = '0' ; c <= '9' ; c++)
+{
+putchar(c);
+putchar(', ');
+}
+putchar('\n');
+return (0);
+}
