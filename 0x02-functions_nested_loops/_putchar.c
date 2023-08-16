3c3e03c97f21b17103746c580552f80d7a88dcaf
haneenakram <nonakram@gmail.com>
2023-08-16 15:39:20 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *_putchar - write the character c to sdout
+  *@c: the character to print
+  *Return: on success 1,
+  *on error, -1 is returned and error is set appropiately
+  */
+int _putchar(char c)
+{
+	return (write(1, &c 1));
+}
