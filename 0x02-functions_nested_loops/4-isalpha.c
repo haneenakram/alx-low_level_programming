466e080c8ff4e61c1d1c73e7e79c15523f890b40
haneenakram <nonakram@gmail.com>
2023-08-16 16:38:05 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_isalpha - checks if letter is upper or lower
+*@c: takes input from other functions
+*Return: return 1 if true else 0
+*/
+int _isalpha(int c)
+{
+if (c <= 'z' && c >= 'a' || c <= 'Z' && c >= 'A')
+{
+return (1);
+}
+else 
+return (0);
+}
