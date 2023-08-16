5270272b8a5f360ae7b403b36f2b173785c772eb
haneenakram <nonakram@gmail.com>
2023-08-16 16:12:31 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,9 +3,9 @@ include "main.h"
 void print_alphabet_x10(void)
 {
 int i, cnt = 1;
-while(cnt <= 10)
+while (cnt <= 10)
 {
-for(i = 'a'; i <= 'z'; i++)
+for (i = 'a'; i <= 'z'; i++)
 {
 	_putchar(i);
 }
