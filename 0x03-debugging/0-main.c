dde1a298a880505e2081e994906d40ab618958da
haneenakram <nonakram@gmail.com>
2023-08-17 09:40:14 -0700

    0-main.c, main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,11 @@
+#include <stdio>
+#include "main.h"
+void positive_or_negative(int i)
+{
+if (i < 0)
+	printf("%d is negative", i);
+else if (i > 0)
+ptintf("%d is positive", i);
+else
+	ptintf("%d is zero", i);
+}
