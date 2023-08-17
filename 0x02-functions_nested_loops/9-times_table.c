1ec07c54c57cd608b4e9a3527cf9b3336d00ed30
haneenakram <nonakram@gmail.com>
2023-08-16 18:36:37 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*jack_bauer - prints every minute of day
+*/
+void jack_bauer(void)
+{
+int m, h, x;
+for (h = 0; h <= 9; h++)
+{
+for (m = 0; m <= 9; m++)
+{
+x = m * n;
+if (x <= 9)
+_putchar(' ');
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(x / 10 + '0');
+_putchar(x % 10 + '0');
+}
+}
+_putchar('\n');
+}
+}
