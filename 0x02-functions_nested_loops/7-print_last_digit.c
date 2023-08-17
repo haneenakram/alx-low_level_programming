d644a40230c54f8a0e089d0f1c0b615e46661b95
haneenakram <nonakram@gmail.com>
2023-08-16 17:43:44 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*print_last_digit - computes the last digit
+*@n: takes type of input
+*Return: always 0
+*/
+int print_last_digit(int n)
+{
+int x;
+x = n % 10;
+return (x);
+}
