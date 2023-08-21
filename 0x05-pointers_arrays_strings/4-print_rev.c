788907e6220aa1738d5b765dca0c4a5da0f7387d
haneenakram <nonakram@gmail.com>
2023-08-21 15:08:21 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,18 +6,16 @@
 */
 void rev_string(char *s)
 {
-	char tmp;
-	int i, len = 0, len1 = 0;
+int i = 0;
 
-	while (s[len] != '\0')
+	while (s[i])
 	{
-		len++;
+		i++;
 	}
-	len1 = len - 1;
-	for (i = 0; i < len / 2; i++)
+	
+	while (i--)
 	{
-		tmp = s[i];
-		s[i] = s[len1];
-		s[len1--] = tmp;
+		_putchar(s[i]);
 	}
+	_putchar('\n');
 }
