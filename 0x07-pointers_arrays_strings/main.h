0a93929ecba5a5b8c88799ac21d2a09b37537cca
haneenakram <nonakram@gmail.com>
2023-08-28 17:56:47 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strstr(char *haystack, char *needle);
