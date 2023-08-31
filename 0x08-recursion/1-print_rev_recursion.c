6c67c0e707603b2f3dceafa6f0b3d83ec36674e2
haneenakram <nonakram@gmail.com>
2023-08-30 17:49:32 -0700

    2-strlen_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
+#include "holberton.h"
 
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
-		putchar(*s);
+		_putchar(*s);
 	}
-}
-int main(void)
-{
-    _print_rev_recursion("\nColton Walker");
-    return (0);
 }
